df8adc147283a8f65383d72905bd70cb64aa316e
PathyFernandez <116598443+Pathyfernandez@users.noreply.github.com>
2023-05-25 14:08:39 -0400

    Clase7

---
+++ Clase5/biblioteca/Clase7/Barra de navegación con Flex/JS/index.js
@@ -0,0 +1,11 @@
+function mensaje (nombre){
+    console.log("Hola" + nombre);
+    console.log("Estas a punto de cerrar sesión");
+    alert("Hola "+nombre);
+    
+}
+
+function agregarContador () {
+    contador ++;
+    console.log(contador);
+}
